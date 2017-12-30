907ee08873d14cefdc5c225f2a22157fab2c4cea
Duncan Thacker <duncan.thacker@atropos.co.uk>
2017-12-30 15:13:07 +0000

    tweak timeline style add expanding/collapsing timeline entries

--- src/PatientTimeline.js
+++ src/PatientTimeline.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import createClass from 'create-react-class';
+import { Collapse } from 'react-collapse';
 import moment from 'moment';
 
 const PLAIN_LIST_STYLE = {
@@ -17,6 +19,11 @@ const timeline = [{
     summary: "Blood Pressure Reading 120/80",
     source: "Blood Pressure machine 12aa312",
     timestamp: "2017-12-29T12:25:00"
+},{
+    id: 12347,
+    summary: "Nursing notes",
+    source: "A. N. Urse",
+    timestamp: "2017-12-28T08:10:00"
 }];
 
 const TIMELINE_ENTRY_STYLE = {
@@ -30,24 +37,52 @@ const LIGHT_STYLE = {
     marginLeft: "16px"
 };
 
-function TimelineEntry( { timestamp, summary, source } ) {
+function TimelineEntry( { timestamp, summary, source, id, onClick, isOpened } ) {
+    const className = isOpened ? "timeline-entry is-opened" : "timeline-entry";
     return (
-        <li class="timeline-entry" style={ TIMELINE_ENTRY_STYLE }>
-            <h3>
+        <li class={ className } style={ TIMELINE_ENTRY_STYLE } onClick={ () => onClick( id ) }>            
+            <h3 style={ { padding: 0, margin: "4px 0px" } }>
                 { summary }
                 <span style={ LIGHT_STYLE }>{ source }</span>
             </h3>
-            { moment( timestamp ).fromNow() }
+            <div style={ { fontSize: "80%" } }>{ moment( timestamp ).fromNow() }</div>
+            <Collapse isOpened={ isOpened }>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Collapse>
         </li>
     );
 }
 
-export default function PatientTimeline({ style }) {
-    return (
-        <div style={ style }>
-            <ul style={ PLAIN_LIST_STYLE }>
-                { timeline.map( timelineItem => <TimelineEntry key={ timelineItem.id } { ...timelineItem } /> ) }
-            </ul>
-        </div>
-    );
-}
+const PatientTimeline = createClass({
+    getInitialState() {
+        return { openEntry: false };
+    },
+    toggleEntryOpen( entryId ) {
+        this.setState( ( previousState ) => {
+            const alreadyOpen = previousState.openEntry === entryId;
+            return {
+                openEntry: alreadyOpen ? false : entryId
+            };
+        });
+    },
+    render() {
+        return (
+            <div style={ this.props.style }>
+                <ul style={ PLAIN_LIST_STYLE }>
+                    { 
+                        timeline.map( timelineItem => 
+                            <TimelineEntry 
+                                key={ timelineItem.id } 
+                                isOpened={ this.state.openEntry === timelineItem.id } 
+                                onClick={ this.toggleEntryOpen } 
+                                { ...timelineItem } 
+                            /> 
+                        ) 
+                    }
+                </ul>
+            </div>
+        );
+    }
+});
+
+export default PatientTimeline;
